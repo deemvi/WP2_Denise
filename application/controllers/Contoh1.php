2c7ea37d447e09277dd3bd6440fb7824fefb1ea4
deemvi <deemvi25@gmail.com>
2022-09-18 14:21:58 +0700

    menambahkan contoh1 pada ci di github

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+       echo "<h1>perkenalkan</h1>";
+        echo "Nama saya Imam Nawawi Saya tingga di daerah Ciputat olah raga yang saya sukai adalah Bulutangkis";
+    }
+}
